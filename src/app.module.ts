46d2165f812beb1cd9a2c3e0a8bc32ec6a3e9a9c
Joel Lopez Villarreal <joell@southern.edu>
2023-04-05 17:29:26 -0400

    Configured Mikro-orm and cross-env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LodgingModule } from './lodging/lodging.module';
-import { UserModule } from './user/user.module';
-import { TrailModule } from './trail/trail.module';
-import { TripModule } from './trip/trip.module';
-import { TransportationModule } from './transportation/transportation.module';
-import { EquipmentSetModule } from './equipment-set/equipment-set.module';
 import { UsersModule } from './users/users.module';
 import { TripsModule } from './trips/trips.module';
 import { LodgingsModule } from './lodgings/lodgings.module';
@@ -15,15 +9,17 @@ import { TransportationsModule } from './transportations/transportations.module'
 import { EquipmentSetsModule } from './equipment-sets/equipment-sets.module';
 import { EquipmentItemsModule } from './equipment-items/equipment-items.module';
 import { EquipmentItemModule } from './equipment-item/equipment-item.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
     imports: [
-        LodgingModule,
-        UserModule,
-        TrailModule,
-        TripModule,
-        TransportationModule,
-        EquipmentSetModule,
+        MikroOrmModule.forRoot(),
+        LodgingsModule,
+        UsersModule,
+        TrailsModule,
+        TripsModule,
+        TransportationsModule,
+        EquipmentSetsModule,
         UsersModule,
         TripsModule,
         LodgingsModule,
